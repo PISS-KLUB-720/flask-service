0925edc3465727b125f9cdb60220e70d488fc67c
babogdanov <boyan.bogdanov@accedia.com>
2023-01-08 13:07:56 +0200

    Add JSON shape example and stub POST request for a specific player

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/player", methods=["POST"])
+def get_player_data():
+    print(request.data)
+    return {"position": "right"}
